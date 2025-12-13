da7b716b335577b217a14b508f228d52d5155591
Aanchal Yadav <yadavaanchal2005@gmail.com>
2025-12-13 22:40:57 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/globals.css";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
+import { ChatProvider } from "./context/ChatContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ThemeProvider>
-    <App />
+    <ChatProvider>
+      <App />
+    </ChatProvider>
   </ThemeProvider>
 );
