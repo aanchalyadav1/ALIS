e93b9a5aaf2882534ec9f44de7232229a3ad437e
Aanchal Yadav <yadavaanchal2005@gmail.com>
2025-12-13 22:57:26 +0530

    Create TypingIndicator.jsx

---
+++ Frontend/src/components/chat/TypingIndicator.jsx
@@ -0,0 +1,7 @@
+export default function TypingIndicator() {
+  return (
+    <div className="flex justify-center my-4">
+      <img src="/wings_bg.png" className="h-10 animate-spin-slow" />
+    </div>
+  );
+}
