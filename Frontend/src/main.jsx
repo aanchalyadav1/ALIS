a11d69f1158cdbcac39bb5bfb2f7e8597d789a7b
Aanchal Yadav <yadavaanchal2005@gmail.com>
2025-12-14 23:40:25 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,17 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/globals.css";
-
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 import { ChatProvider } from "./context/ChatContext.jsx";
 import { LoanSessionProvider } from "./context/LoanSessionContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ThemeProvider>
-    <LoanSessionProvider>
-      <ChatProvider>
+    <ChatProvider>
+      <LoanSessionProvider>
         <App />
-      </ChatProvider>
-    </LoanSessionProvider>
+      </LoanSessionProvider>
+    </ChatProvider>
   </ThemeProvider>
 );
