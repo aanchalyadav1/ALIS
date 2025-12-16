a7a1723ff0656e3fa44c844289e8090c429db61b
Aanchal Yadav <yadavaanchal2005@gmail.com>
2025-12-16 18:11:32 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -10,14 +10,14 @@ import { AuthProvider } from "./context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <ThemeProvider>
-        <LoanSessionProvider>
-          <ChatProvider>
-            <App />
-          </ChatProvider>
-        </LoanSessionProvider>
-      </ThemeProvider>
-    </AuthProvider>
+<AuthProvider>
+  <ThemeProvider>
+    <LoanSessionProvider>
+      <ChatProvider>
+        <App />
+      </ChatProvider>
+    </LoanSessionProvider>
+  </ThemeProvider>
+</AuthProvider>
   </React.StrictMode>
 );
