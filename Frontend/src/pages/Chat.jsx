39129eaa7fcd3231bda4b915ecd4e2ccb759605d
Aanchal Yadav <yadavaanchal2005@gmail.com>
2025-12-16 18:33:27 +0530

    Update Chat.jsx

--- Frontend/src/pages/Chat.jsx
+++ Frontend/src/pages/Chat.jsx
@@ -2,7 +2,7 @@ import ChatLayout from "../components/chat/ChatLayout";
 
 export default function Chat() {
   return (
-    <div className="min-h-[calc(100vh-64px)] bg-[#05060a]">
+    <div className="min-h-screen bg-[#05060a]">
       <ChatLayout />
     </div>
   );
