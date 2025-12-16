22991080bb93f3bdbfbe7167bb1fd82b3f2e3d2f
Aanchal Yadav <yadavaanchal2005@gmail.com>
2025-12-16 19:00:28 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,18 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <div
-    style={{
-      color: "white",
-      background: "#05060a",
-      height: "100vh",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      fontSize: "32px",
-    }}
-  >
-    REACT IS WORKING
-  </div>
+  <App />
 );
